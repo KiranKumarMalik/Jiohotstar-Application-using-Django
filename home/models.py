cac9c6a530d0f4008ae07cd8633eea7a7a570eec
KiranKumarMalik <malikkiran413@gmail.com>
2025-03-10 11:36:00 +0530

    new feature

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class BannerCard(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
     content = models.TextField()
-    imageurl = models.CharField(max_length=1000)
+    imageurl = models.ImageField(upload_to='images/%Y/%m/%d/')
     targetUrl = models.CharField(max_length=1000)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class BannerCard(models.Model):
 class Card(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
-    imageurl = models.CharField(max_length=1000)
+    imageurl = models.ImageField(upload_to='images/%Y/%m/%d/')
     targetUrl = models.CharField(max_length=1000)
 
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.models import auth
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    bannerCard = BannerCard.objects.all()
+    context = {
+        'bannerCard': bannerCard,
+    }
+    return render(request, "Home/home.html", context)
