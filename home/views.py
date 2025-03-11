31669a9338f29e1d5ef9edf61a1316cb3ace951d
KiranKumarMalik <malikkiran413@gmail.com>
2025-03-11 21:37:09 +0530

    change in code

--- home/views.py
+++ home/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import auth
 # Create your views here.
 def home(request):
     bannerCard = BannerCard.objects.all()
+    card = Card.objects.all()
     context = {
         'bannerCard': bannerCard,
+        'card': card,
     }
     return render(request, "Home/home.html", context)
